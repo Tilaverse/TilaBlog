e155dab3788f431b68d7cc3140c31f4d8c844686
Tilaverse <158148969+Tilaverse@users.noreply.github.com>
2025-11-13 17:44:32 +0100

    Delete astro-blog directory

--- astro-blog/astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://your-username.github.io/your-repo-name', // set correctly
-  base: '/', // or '/your-repo-name/' if needed
-});
